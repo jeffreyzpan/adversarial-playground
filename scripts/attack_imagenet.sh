92d8f6a284d4691bb991f93e0fbfc6f36f307cfd
Jeffrey Pan <jeffreyzpan@gmail.com>
2020-05-16 11:44:49 -0400

    Experiment script modifications

--- scripts/attack_imagenet.sh
+++ scripts/attack_imagenet.sh
@@ -12,4 +12,4 @@ EPS=$3
 INPUT_SIZE=$4
 CONTRAST=$5
 
-python run_adv.py --dataset ${dataset} --arch ${ARCH} --gpu_ids ${GPU} --resume ./checkpoints/contrast_${5}_${4}_${dataset}_${ARCH}_${train_epochs}/model_best.pth.tar --save_path /nobackup/users/jzpan/attack_logs/imagenet/contrast_${5}_${4}_${2}_${3}_comp_defences_${dataset}_${ARCH}_${train_epochs} --epochs ${adv_epochs} --attacks ${ATTACK} --defences jpeg tvm --epsilons ${3} --input_size ${4} --inc_contrast ${5} --batch_size 32
+python run_adv.py --dataset ${dataset} --arch ${ARCH} --gpu_ids ${GPU} --resume ./checkpoints/contrast_${5}_${4}_${dataset}_${ARCH}_${train_epochs}/model_best.pth.tar --save_path /nobackup/users/jzpan/attack_logs/imagenet/contrast_${5}_${4}_${2}_${3}_comp_defences_${dataset}_${ARCH}_${train_epochs} --epochs ${adv_epochs} --attacks ${ATTACK} --defences jpeg tvm --epsilons ${3} --input_size ${4} --inc_contrast ${5}
